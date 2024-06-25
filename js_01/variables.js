c2edd3b3d910699125a0df23a8f38e13aa31698d
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-06-25 19:03:39 +0000

    variables conmmit

---
+++ js_01/variables.js
@@ -0,0 +1,12 @@
+const id=111;
+let email="mayankshahi@gmail.com";
+var status="present";
+city="Delhi"; 
+// city="Delhi";    bad practice    
+// let new_value;   undefined
+
+console.log(id);
+
+console.table([id,email,city,status]);
+
+console.log(new_value);
