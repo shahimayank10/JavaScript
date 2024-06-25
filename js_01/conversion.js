6e6dc0e7877269d00b7269017fe6c7958b6d1de8
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-06-25 20:06:55 +0000

    conversion  commit

---
+++ js_01/conversion.js
@@ -0,0 +1,12 @@
+let score="10";
+
+console.log(typeof(score));
+
+let update=Number(score);
+
+console.log(typeof(update));
+console.log(update);
+
+
+// let score=null;          update->   0
+// let score=undefine;      update->   NaN
