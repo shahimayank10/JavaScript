7c8d71f0001851a0ebb26bab1b17d38ee91eec9f
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-06-25 19:28:09 +0000

    commit datatype

---
+++ js_01/datatype.js
@@ -0,0 +1,14 @@
+// datatypes
+
+//  1. number            -(2^53)-1  to  (2^53)-1
+//  2. NULL              standalone value
+//  3. Undefined
+//  4. bigint
+//  5. string
+//  6. symbol             unique
+
+
+
+console.log(typeof null);
+
+console.log(typeof undefined);
