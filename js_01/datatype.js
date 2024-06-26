3c68797553bd6215d9e59d203b2bc251b3a0d0b1
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-06-26 19:38:17 +0000

    commit datatype

--- js_01/datatype.js
+++ js_01/datatype.js
@@ -1,5 +1,10 @@
 // datatypes
 
+
+// 2 types
+
+
+// <<<<< Premitive >>>>>>(call by number)
 //  1. number            -(2^53)-1  to  (2^53)-1
 //  2. NULL              standalone value
 //  3. Undefined
@@ -9,6 +14,13 @@
 
 
 
-console.log(typeof null);
+console.log(typeof null);    
 
 console.log(typeof undefined);
+
+
+// <<<<< non-Premitive/Reference >>>>>>(call by refrence)
+
+// 1.Array
+// 2.object
+// 3.functions
