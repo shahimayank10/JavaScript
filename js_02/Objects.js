5c1485f6bab33230a0da098820328bba5828b234
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-07-07 15:05:02 +0000

    object1 commit

--- js_02/Objects.js
+++ js_02/Objects.js
@@ -1,8 +1,11 @@
 // Object Literals
 
+const sym=Symbol("k")
+
 const obj={
     name:"mayank",
     college:"IMS",
+    [sym]:"keys",
     Subject:"js",
     age:21
 }
@@ -16,3 +19,13 @@ console.log(obj);
 console.log(obj["name"]);
 
 console.log(obj.name);
+
+console.log(obj[sym]);     //only way to access symbol
+
+
+Object.freeze(obj)
+newobj.age=22;
+
+// no change in age...both object got freeze
+console.log(obj);
+console.log(newobj);
