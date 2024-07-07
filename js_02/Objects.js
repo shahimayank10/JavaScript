8e39039c1fefaf86de9eadf2b8014a4e807494a0
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-07-07 14:10:58 +0000

    obj

---
+++ js_02/Objects.js
@@ -0,0 +1,18 @@
+// Object Literals
+
+const obj={
+    name:"mayank",
+    college:"IMS",
+    Subject:"js",
+    age:21
+}
+
+const newobj=obj;
+
+newobj.name="prateek";
+
+console.log(obj);
+
+console.log(obj["name"]);
+
+console.log(obj.name);
