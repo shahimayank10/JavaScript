89de5e5acdcdba393be832856cb71fbf77419a88
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-07-12 18:45:16 +0000

    map commit

--- js_04/iterators.js
+++ js_04/iterators.js
@@ -35,14 +35,44 @@ const obj = {
     "property": 9, 
   };
 
-for(let d in obj){
-    console.log(d);
-	console.log(obj[d]);
-}
+// for(let d in obj){
+//     console.log(d);
+// 	console.log(obj[d]);
+// }
 
-for(let d of data){
-    console.log(d);   
-}
+// for(let d of data){
+//     console.log(d);   
+// }
 
 
 // -------------------maps-----------------------
+// store value in key-value pairs
+// can't iterate (for-in loop don't work with no error)
+// in for-of loop key,value pair can define
+
+
+console.log(typeof Map);    //function
+
+const map=new Map();         
+console.log(typeof map);       //object
+
+map.set('cpp',"c++");
+map.set('rb',"ruby");
+map.set('js',"javascript");
+
+// console.log(map); 
+// Map(3) { 'cpp' => 'c++', 'rb' => 'ruby', 'js' => 'javascript' }
+
+for(let i of map){
+	console.log(i);
+} 
+// [ 'cpp', 'c++' ]
+// [ 'rb', 'ruby' ]
+// [ 'js', 'javascript' ]
+
+for(let [key,value] of map){
+	console.log(key, value);
+}
+//  cpp c++
+//  rb ruby
+//  js javascript
