ae7ae8dec4e6799d9dc4e91ee4e311b8006c7e57
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-07-12 14:39:54 +0000

    commit

---
+++ js_04/iterators.js
@@ -0,0 +1,47 @@
+const data=[
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+];
+
+// const obj = {
+//     "property1": "value1", 
+//     "property2": "value2", 
+//     "property": 9, 
+//   };
+
+for(let d in data){
+    console.log(d);
+}
+
+for(let d of data){
+    console.log(d);
+}
+
+
+// -------------------maps-----------------------
