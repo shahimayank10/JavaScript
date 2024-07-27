78e449f67f9f8c2796447a55945be5c7b2e28403
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-07-27 18:14:10 +0000

    prototype property commit

---
+++ advancejs/prototype.js
@@ -0,0 +1,22 @@
+function multiply(num){
+return num*6;
+}
+
+// console.log(multiply.prototype);             output:{}
+
+function user(name,age){
+    this.name=name;
+    this.age=age;
+}
+
+
+user.prototype.print=function(){
+    console.log(`age of ${this.name} is ${this.age} `);
+    // return `age of ${this.name} is ${this.age} `;
+}
+
+const userone=new user("mayank",21)
+const usertwo=new user("prateek",21)
+
+console.log(userone.print());
+console.log(multiply.prototype);  
