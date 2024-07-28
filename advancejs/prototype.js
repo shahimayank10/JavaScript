842db12e86286cd9b7010b0dff0436a1a957054c
Mayank Shahi <140376464+shahimayank10@users.noreply.github.com>
2024-07-28 08:07:00 +0000

    proto commit

--- advancejs/prototype.js
+++ advancejs/prototype.js
@@ -1,22 +1,39 @@
-function multiply(num){
-return num*6;
+// function multiply(num){
+// return num*6;
+// }
+
+// // console.log(multiply.prototype);             output:{}
+
+// function user(name,age){
+//     this.name=name;
+//     this.age=age;
+// }
+
+
+// user.prototype.print=function(){
+//     console.log(`age of ${this.name} is ${this.age} `);
+//     // return `age of ${this.name} is ${this.age} `;
+// }
+
+// const userone=new user("mayank",21)
+// const usertwo=new user("prateek",21)
+
+// console.log(userone.print());
+// console.log(multiply.prototype);  
+
+
+const engine={
+    horcepower:220,
+    topspeed:300
 }
 
-// console.log(multiply.prototype);             output:{}
-
-function user(name,age){
-    this.name=name;
-    this.age=age;
+const audiR8={
+    color:"yellow"
 }
 
+console.log(audiR8.color)
 
-user.prototype.print=function(){
-    console.log(`age of ${this.name} is ${this.age} `);
-    // return `age of ${this.name} is ${this.age} `;
-}
+audiR8.__proto__=engine;
 
-const userone=new user("mayank",21)
-const usertwo=new user("prateek",21)
-
-console.log(userone.print());
-console.log(multiply.prototype);  
+console.log(audiR8.horcepower)  
+console.log(audiR8.topspeed)  
